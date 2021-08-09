4c4f7a87d025397e7184a718e22190646bf5b52e
ord0077 <ord0077@gmail.com>
2021-08-09 18:09:50 +0500

    fmr changes

--- src/pages/main/reports/fundManagerReports/fundManagerReports.ts
+++ src/pages/main/reports/fundManagerReports/fundManagerReports.ts
@@ -8,7 +8,7 @@ import { InAppBrowser } from '@ionic-native/in-app-browser';
 export class FundManagerReports {
   reports;
   constructor(public app: App, private http: HttpClient , private iab: InAppBrowser) {
-    this.http.get('http://orangeroomdigital.com/fmrjson/fmr.php').subscribe(data => {
+    this.http.get('https://orangeroomdigital.com/fmrjson/fmr.php').subscribe(data => {
       this.reports = data;
     });
   }
